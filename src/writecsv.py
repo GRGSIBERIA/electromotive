6847b9f3ea66dd0288ac61f4920d3eb7aff25001
grgsiberia <nanashi4129@gmail.com>
2019-09-21 18:47:52 +0900

    CSV書き出し関数を作る

--- src/writecsv.py
+++ src/writecsv.py
@@ -1,5 +1,23 @@
 #-*- encoding: utf-8
 
-class CSVWriter:
-    def __init__(self, js, magnets):
-        pass
+def writecsv(js, timemagnets):
+    header = "time"
+
+    for part, conf in js.items():
+        if "config" in part:
+            continue
+
+        if "magnet" in conf["type"]:
+            header += "," + part
+    
+    with open(js["config"]["csv"], "w") as f:
+        f.write(header + "\n")
+
+        times = js["config"]["times"]
+
+        for ti, magnets in enumerate(timemagnets):
+            rs = str(times[ti])
+            for mag in magnets:
+                rs += "," + str(mag.inducedvoltage)
+            f.write(rs + "\n")
+        
